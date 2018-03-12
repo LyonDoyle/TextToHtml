ae3c65bb4545b7a1eb95c4743c137cad4553bfe4
Lyon <lyondoyle@live.com>
2018-03-12 00:00:33 -0600

    Initial check-in

---
+++ TextToHtmlFormatter/frmTextToHtml.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace TextToHtmlFormatter
+{
+    partial class frmTextToHtml
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnGenerateSampleValues = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtExample = new System.Windows.Forms.TextBox();
+            this.txtDefinition = new System.Windows.Forms.TextBox();
+            this.txtTerm = new System.Windows.Forms.TextBox();
+            this.txtHeading = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.webBrowser);
+            this.splitContainer1.Size = new System.Drawing.Size(978, 577);
+            this.splitContainer1.SplitterDistance = 326;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnGenerateSampleValues);
+            this.panel1.Controls.Add(this.btnClear);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnDone);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.txtExample);
+            this.panel1.Controls.Add(this.txtDefinition);
+            this.panel1.Controls.Add(this.txtTerm);
+            this.panel1.Controls.Add(this.txtHeading);
+            this.panel1.Controls.Add(this.menuStrip1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(326, 577);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnGenerateSampleValues
+            // 
+            this.btnGenerateSampleValues.Location = new System.Drawing.Point(31, 455);
+            this.btnGenerateSampleValues.Name = "btnGenerateSampleValues";
+            this.btnGenerateSampleValues.Size = new System.Drawing.Size(267, 48);
+            this.btnGenerateSampleValues.TabIndex = 12;
+            this.btnGenerateSampleValues.Text = "Create Sample";
+            this.btnGenerateSampleValues.UseVisualStyleBackColor = true;
+            this.btnGenerateSampleValues.Click += new System.EventHandler(this.btnGenerateSampleValues_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(31, 413);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(117, 36);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Reset All";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Example";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Definition";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Term";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Heading";
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(181, 413);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(117, 36);
+            this.btnDone.TabIndex = 5;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(223, 362);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtExample
+            // 
+            this.txtExample.Location = new System.Drawing.Point(31, 287);
+            this.txtExample.Multiline = true;
+            this.txtExample.Name = "txtExample";
+            this.txtExample.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtExample.Size = new System.Drawing.Size(267, 69);
+            this.txtExample.TabIndex = 3;
+            // 
+            // txtDefinition
+            // 
+            this.txtDefinition.Location = new System.Drawing.Point(31, 181);
+            this.txtDefinition.Multiline = true;
+            this.txtDefinition.Name = "txtDefinition";
+            this.txtDefinition.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtDefinition.Size = new System.Drawing.Size(267, 69);
+            this.txtDefinition.TabIndex = 2;
+            // 
+            // txtTerm
+            // 
+            this.txtTerm.Location = new System.Drawing.Point(31, 124);
+            this.txtTerm.Name = "txtTerm";
+            this.txtTerm.Size = new System.Drawing.Size(267, 20);
+            this.txtTerm.TabIndex = 1;
+            // 
+            // txtHeading
+            // 
+            this.txtHeading.Location = new System.Drawing.Point(31, 67);
+            this.txtHeading.Name = "txtHeading";
+            this.txtHeading.Size = new System.Drawing.Size(267, 20);
+            this.txtHeading.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(326, 24);
+            this.menuStrip1.TabIndex = 11;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(648, 577);
+            this.webBrowser.TabIndex = 1;
+            // 
+            // frmTextToHtml
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(978, 577);
+            this.Controls.Add(this.splitContainer1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmTextToHtml";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmTextToHtml_FormClosing);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtExample;
+        private System.Windows.Forms.TextBox txtDefinition;
+        private System.Windows.Forms.TextBox txtTerm;
+        private System.Windows.Forms.TextBox txtHeading;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnGenerateSampleValues;
+        private System.Windows.Forms.WebBrowser webBrowser;
+    }
+}
+
---
+++ TextToHtmlFormatter/frmTextToHtml.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TextToHtmlFormatter
+{
+    public partial class frmTextToHtml : Form
+    {
+        #region CONSTANTS
+        const string DECLARATION = "<!DOCTYPE html>";
+        const string HTML_OPENING_TAG = "<html lang=\"en\">";
+        const string HTML_CLOSING_TAG = "</html>";
+        const string TITLE_OPENING_TAG = "<title>";
+        const string TITLE_CLOSING_TAG = "</title>";
+        const string META_CHARSET_TAG = "<meta charset=\"utf-8\">";
+        const string HEAD_OPENING_TAG = "<head>";
+        const string HEAD_CLOSING_TAG = "</head>";
+        const string BODY_OPENING_TAG = "<body>";
+        const string BODY_CLOSING_TAG = "</body>";
+        const string H1_OPENING_TAG = "<h1>";
+        const string H1_CLOSING_TAG = "</h1>";
+        const string PARAGRAPH_OPENING_TAG = "<p>";
+        const string PARAGRAPH_CLOSING_TAG = "</p>";
+        const string STRONG_OPENING_TAG = "<strong>";
+        const string STRONG_CLOSING_TAG = "</strong>";
+        const string EXAMPLE_FORMAT_STRING = "<blockquote>Ex. <em>\"{0}\"</em></blockquote>";
+        const string TAB = "\t";
+        const string NEWLINE = "\n";
+        const string EMPTY_STRING = "";
+        const string UNSAVED_CHANGES_MESSAGE = "You have unsaved changes. Continue without saving?";
+        const string UNSAVED_CHANGES_CAPTION = "Confirm";
+        List<ListEntry> _listEntries;
+        #endregion CONSTANTS
+
+        bool TESTMODE = false;
+        string _htmlAsString;
+        bool _changesSaved = true;
+
+        public frmTextToHtml()
+        {
+            InitializeComponent();
+
+            Text = "Text To HTML";
+            AcceptButton = btnAdd;
+            CancelButton = btnDone;
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            _listEntries = new List<ListEntry>();
+
+            webBrowser.AllowWebBrowserDrop = false;
+            webBrowser.WebBrowserShortcutsEnabled = false;
+            webBrowser.IsWebBrowserContextMenuEnabled = false;
+            webBrowser.ScriptErrorsSuppressed = true;
+
+            btnGenerateSampleValues.Enabled = TESTMODE;
+            btnGenerateSampleValues.Visible = TESTMODE;
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string title = txtHeading.Text;
+            string term = txtTerm.Text;
+            string definition = txtDefinition.Text;
+            string example = txtExample.Text;
+
+            if (_listEntries == null)
+                _listEntries = new List<ListEntry>();
+
+            var listEntry = createNewEntry(term, definition, example);
+            if (listEntry != null)
+                _listEntries.Add(listEntry);
+
+            clear(false);
+
+            _htmlAsString = formatList(_listEntries, title) ?? EMPTY_STRING;
+            _changesSaved = false;
+            webBrowser.DocumentText = _htmlAsString;
+            txtTerm.Select();
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(_htmlAsString))
+                return;
+
+            saveToFile(_htmlAsString);
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            clear();
+        }
+        private void btnDone_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void btnGenerateSampleValues_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                var listEntry = new ListEntry();
+                listEntry.GenerateSampleValues();
+
+                _listEntries.Add(listEntry);
+            }
+
+            btnAdd_Click(null, EventArgs.Empty);
+        }
+        private void frmTextToHtml_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_changesSaved == false)
+            {
+                DialogResult result = MessageBox.Show(UNSAVED_CHANGES_MESSAGE, UNSAVED_CHANGES_CAPTION, MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (result == DialogResult.No)
+                    e.Cancel = true;
+            }
+        }
+
+        /// <summary>
+        /// Returns a new stand-alone HTML5 document with all ListEntry items formatted as seperate paragraphs in a style similar to a definition list, 
+        /// however with each definition having an example as well. 
+        /// </summary>
+        /// <param name="list">The complete list of all ListEntry items to be included in the HTML document.</param>
+        /// <param name="title">Title of the HTML document.</param>
+        /// <returns>The formatted, stand-alone, HTML5 document as a string.</returns>
+        private string formatList(List<ListEntry> list, string title)
+        {
+            bool noTitle = string.IsNullOrWhiteSpace(title);
+            bool noList = list == null || list.Count == 0;
+
+            if (noList)
+                return null;
+
+            string listAsHtml = DECLARATION + NEWLINE;
+            listAsHtml += HTML_OPENING_TAG + NEWLINE + NEWLINE;
+            listAsHtml += HEAD_OPENING_TAG + NEWLINE;
+            listAsHtml += TAB + META_CHARSET_TAG + NEWLINE;
+            listAsHtml += TAB + $"{TITLE_OPENING_TAG}{title ?? EMPTY_STRING}{TITLE_CLOSING_TAG}" + NEWLINE;
+            listAsHtml += HEAD_CLOSING_TAG + NEWLINE + NEWLINE;
+            listAsHtml += BODY_OPENING_TAG + NEWLINE;
+            if (!noTitle)
+                listAsHtml += TAB + $"{H1_OPENING_TAG}{title}{H1_CLOSING_TAG}" + NEWLINE;
+
+            foreach (ListEntry entry in list)
+            {
+                if (entry == null)
+                    continue;
+
+                bool noTerm = string.IsNullOrWhiteSpace(entry.Term);
+                bool noDefinition = string.IsNullOrWhiteSpace(entry.Definition);
+                bool noExample = string.IsNullOrWhiteSpace(entry.Example);
+
+                if (noTerm)
+                    continue;
+
+                listAsHtml += TAB + PARAGRAPH_OPENING_TAG + NEWLINE;
+                listAsHtml += TAB + TAB + $"{STRONG_OPENING_TAG}{entry.Term}{STRONG_CLOSING_TAG}";
+                if (!noDefinition)
+                    listAsHtml += $"{TAB}{entry.Definition}" + NEWLINE;
+                if (!noExample)
+                    listAsHtml += TAB + TAB + string.Format(EXAMPLE_FORMAT_STRING, entry.Example) + NEWLINE;
+
+                listAsHtml += TAB + PARAGRAPH_CLOSING_TAG + NEWLINE;
+            }
+
+            listAsHtml += BODY_CLOSING_TAG + NEWLINE + NEWLINE;
+            listAsHtml += HTML_CLOSING_TAG + NEWLINE;
+
+            return listAsHtml;
+        }
+        /// <summary>
+        /// Clears all of this forms controls, including textboxes, list, and web browser.
+        /// </summary>
+        private void clear()
+        {
+            clear(true);
+        }
+        /// <summary>
+        /// Clears this forms controls.
+        /// </summary>
+        /// <param name="all">True will clear all this forms controls. False will clear only the controls related to a ListEntry item.</param>
+        private void clear(bool all)
+        {
+            if (all)
+            {
+                _listEntries.Clear();
+                txtHeading.Clear();
+                webBrowser.DocumentText = String.Empty;
+            }
+            txtTerm.Clear();
+            txtDefinition.Clear();
+            txtExample.Clear();
+        }
+        /// <summary>
+        /// Returns a new ListEntry object with the term, definition, and example provided. If any parameter is missing, or contains only space, a null object is returned.
+        /// </summary>
+        /// <param name="term">Technical term to be defined.</param>
+        /// <param name="definition">Definition of this term.</param>
+        /// <param name="example">Example usage of the term.</param>
+        /// <returns>A new ListEntry object, or null object if any parameter was null, empty, or contained only whitespace.</returns>
+        private ListEntry createNewEntry(string term, string definition, string example)
+        {
+            if (string.IsNullOrWhiteSpace(term) || string.IsNullOrWhiteSpace(definition) || string.IsNullOrWhiteSpace(example))
+                return null;
+
+            return new ListEntry()
+            {
+                Term = term,
+                Definition = definition,
+                Example = example
+            };
+        }
+        /// <summary>
+        /// The HTML preview shown will be saved as an HTML document in the location chosen by the user.
+        /// </summary>
+        /// <param name="htmlAsString">The formatted HTML string containing all ListEntry items entered into the form.</param>
+        private void saveToFile(string htmlAsString)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "html files (*.html)|*.html";
+            saveFileDialog.RestoreDirectory = true;
+
+            Stream stream;
+
+            try
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    if ((stream = saveFileDialog.OpenFile()) != null)
+                    {
+                        using (StreamWriter writer = new StreamWriter(stream))
+                        {
+                            writer.Write(htmlAsString);
+                            writer.Flush();
+                        }
+                    }
+                    _changesSaved = true;
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #region Classes
+        class ListEntry
+        {
+            const string ALPHANUMERIC_POOL = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const string EMPTY_STRING = "";
+            static Random _random;
+            string term;
+            string definition;
+            string example;
+
+            /// <summary>
+            /// Any term, its definition, and an example usage of the term.
+            /// </summary>
+            public ListEntry() { }
+
+            /// <summary>
+            /// Any term, its definition, and an example usage of the term.
+            /// </summary>
+            /// <param name="term">Technical term to be defined.</param>
+            /// <param name="definition">Definition of this term.</param>
+            /// <param name="example">Example usage of the term.</param>
+            public ListEntry(string term, string definition, string example)
+            {
+                Term = term;
+                Definition = definition;
+                Example = example;
+            }
+
+            /// <summary>
+            /// This object will be filled with a term, definition, and example of random text.
+            /// </summary>
+            public void GenerateSampleValues()
+            {
+                Term = GenerateRandomString(50);
+                Definition = GenerateRandomString();
+                Example = GenerateRandomString();
+            }
+            /// <summary>
+            /// Returns a string of random alphanumeric characters between 1 and 255 characters long.
+            /// </summary>
+            /// <returns>String of random alphanumeric characters between 1 and 255 characters long.</returns>
+            static string GenerateRandomString()
+            {
+                return GenerateRandomString(Random.Next(1, 256));
+            }
+            /// <summary>
+            /// Returns a string of random alphanumeric characters of the length specified.
+            /// </summary>
+            /// <param name="length">Length of string to create.</param>
+            /// <returns>String of random alphanumeric characters of the length specified.</returns>
+            static string GenerateRandomString(int length)
+            {
+                var chars = Enumerable.Range(0, length)
+                    .Select(x => ALPHANUMERIC_POOL[Random.Next(0, ALPHANUMERIC_POOL.Length)]);
+                return new string(chars.ToArray());
+            }
+
+            static Random Random
+            {
+                get
+                {
+                    if (_random == null)
+                        _random = new Random();
+
+                    return _random;
+                }
+            }
+            /// <summary>
+            /// Technical term to be defined.
+            /// </summary>
+            public string Term
+            {
+                get
+                {
+                    return term ?? EMPTY_STRING;
+                }
+                set
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                        return;
+                    term = value.Trim();
+                }
+            }
+            /// <summary>
+            /// Definition of this term.
+            /// </summary>
+            public string Definition
+            {
+                get
+                {
+                    return definition ?? EMPTY_STRING;
+                }
+                set
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                        return;
+                    definition = value.Trim();
+                }
+            }
+            /// <summary>
+            /// Example usage of the term.
+            /// </summary>
+            public string Example
+            {
+                get
+                {
+                    return example ?? EMPTY_STRING;
+                }
+                set
+                {
+                    if (string.IsNullOrWhiteSpace(value))
+                        return;
+                    example = value.Trim();
+                }
+            }
+        }
+        #endregion Classes
+    }
+}
